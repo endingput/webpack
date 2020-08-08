d99b0fd9176c5aa959d04c289a9dae137fc05fcd
wanghan <1594078165@qq.com>
2020-08-08 23:16:39 +0800

    'webpack'

--- 6.optimize/1.js
+++ 6.optimize/1.js
@@ -1,10 +1,10 @@
-function fn(arr,start, end){
+function fn(arr, start, end) {
     if (typeof start !== "number") return false;
     if (typeof end !== "number") return false;
     arr.push(start);
-    if(start === end) return arr;
-    return fn(arr,start+3, end);
+    if (start === end) return arr;
+    return fn(arr, start + 3, end);
 }
-let arr=[]; 
+let arr = [];
 fn(arr, 3, 9);
 console.log(arr);
