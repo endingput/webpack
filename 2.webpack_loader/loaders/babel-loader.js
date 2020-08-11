cf2c9b16259c1893f529a2a4211e83095985d52c
wanghan <1594078165@qq.com>
2020-08-11 23:26:54 +0800

    'webpack'

--- 2.webpack_loader/loaders/babel-loader.js
+++ 2.webpack_loader/loaders/babel-loader.js
@@ -2,23 +2,28 @@
  * loader其实就 是一个转换函数
  */
 const babel = require('@babel/core');
-function loader(source,inputSourceMap){
+
+function loader(source, inputSourceMap) {
     //babel-loader2!babel-loader!文件路径  ./src/main.js
-    console.log('this.request',this.request);
-    console.log('filename',this.request.split('!').pop().split('\\').pop());
+    console.log('this.request', this.request);
+    console.log('filename', this.request.split('!').pop().split('\\').pop());
     let options = {
-        presets:["@babel/preset-env"],
+        presets: ["@babel/preset-env"],
         inputSourceMap,
-        sourceMaps:true,//告诉 babel也要生成sourceMap文件
+        sourceMaps: true, //告诉 babel也要生成sourceMap文件
         //loader上下文对象(this).request 当前的请路径
         //取出要转换的文件的文件名
-        filename:this.request.split('!').pop().split('/').pop()
+        filename: this.request.split('!').pop().split('/').pop()
     }
     //code转义后的代码 map映射文件 ast抽象语法树
-    let {code,map,ast} = babel.transform(source,options);
+    let {
+        code,
+        map,
+        ast
+    } = babel.transform(source, options);
     //return code;
     //如果我只返回一个值,可以直接return,如果想返回多个值,就可以通过this.callback返回
-    return this.callback(null,code,map,ast);
+    return this.callback(null, code, map, ast);
 }
 
 module.exports = loader;
--- 3.ast/src/index.js
+++ 3.ast/src/index.js
@@ -1,3 +1,6 @@
-import {flatten,concat} from 'lodash';
-console.log(flatten([1,[2,[3,4]]]));
-console.log(concat([1,[2,[3,4]]]));
+import {
+    flatten,
+    concat
+} from 'lodash';
+console.log(flatten([1, [2, [3, 4]]]));
+console.log(concat([1, [2, [3, 4]]]));
