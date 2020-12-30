89e04189b70f5f8434be314271113bd7abc1d377
wanghan <1594078165@qq.com>
2020-12-30 23:26:04 +0800

    'feat:webpack-dev-server用法'

---
+++ 1.basic/note.md
@@ -0,0 +1,16 @@
+## webpack的学习顺序
+- webpack5实战使用
+- webpack优化
+- webpack工作流 ast抽象语法树
+- loader
+- plugin tapable
+- hmr实现原理
+
+##
+- 在现代软件开发中，分包是一个非常重要的思路
+- 把一个大仓库尽可能拆分成不同的小仓库，不同的模块 lerna
+- webpack webpack-cli webpack-dev-serve
+- 就像webpack不会把webpack和webpack-cli进行合并
+
+## webpack-dev-server
+- 使用的是内存文件系统memory-fs
--- 1.basic/webpack.config.js
+++ 1.basic/webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path')
+const resolve = path.resolve
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     devtool: false,
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        path: path.resolve(__dirname, 'a', 'dist'),
+        filename: 'main.js',
+        publicPath: '/assets'
     },
     module: {
         rules: [{
             test: /\.txt$/,
             use: 'raw-loader'
         }]
+    },
+    devServer: {
+        contentBase: resolve(__dirname, 'static'),
+        compress: true, // 是否启动压缩
+        port: 8080,
+        writeToDisk: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
